399878bc1cf8c3de987f483ac539cfd0e2b4dde5
unknown <127o1404@itsx.edu.mx>
2022-03-02 13:35:05 -0600

    version 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# batname
+
+
+Todos los derechos reservados a:
+
+
+This simple little app lets you generate logos like the "The Batman (2022)" film logo. Enjoy!
+
+https://batname.vercel.app
---
+++ src/lib/Disclaimer.svelte
@@ -0,0 +1,19 @@
+<div class="space-y-2">
+  <p class="text-sm text-red-600 uppercase">
+    No tenemos los derechos del logo de "The Batman". Esto es para educación
+    y fines de entretenimiento solamente.
+  </p>
+  <p class="text-sm text-red-600 uppercase">
+    Síguenos:
+  </p>
+  <a
+    class="text-red-600 underline block text-center"
+    href="https://www.instagram.com/dimebruce/"
+    target="_blank">dimebruce</a
+  >
+  <a
+    class="text-red-600 underline block text-center"
+    href="https://twitter.com/_nightwielder_"
+    target="_blank">Shajidhasan</a
+  >
+</div>
